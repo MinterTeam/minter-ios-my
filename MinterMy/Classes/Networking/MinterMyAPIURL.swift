2d8d75837db5fda9f14380bd52dff86d20710892
Alexey Sidorov <ody344@gmail.com>
2018-05-20 23:48:16 +0300

    added folders

--- MinterMy/Classes/Networking/MinterMyAPIURL.swift
+++ MinterMy/Classes/Networking/MinterMyAPIURL.swift
@@ -6,18 +6,41 @@
 //
 
 import Foundation
+import MinterCore
 
-let MinterMyAPIBaseURL = "http://159.89.107.246:8841/api/"
+let MinterMyAPIBaseURL = "http://159.89.107.246:8841/api/v1/"
 
 
 enum MinterMyAPIURL {
 	
-	case balance
+	//
+	case register
+	case login
+	case username
+	//profile
+	case profileConfirm(id: Int)
+	case profile
 	
 	func url() -> URL {
 		switch self {
-		case .balance:
-			return URL(string: MinterMyAPIBaseURL + "getBalance")!
+			
+		//Auth
+		case .register:
+			return URL(string: MinterMyAPIBaseURL + "register")!
+			
+		case .login:
+			return URL(string: MinterMyAPIBaseURL + "login")!
+			
+		case .username:
+			return URL(string: MinterMyAPIBaseURL + "username")!
+			
+		//Profile
+			
+		case .profileConfirm(let id):
+			return URL(string: MinterMyAPIBaseURL + "profile/confirm/\(id)/")!
+			
+		case .profile:
+			return URL(string: MinterMyAPIBaseURL + "profile")!
 			
 		}
 	}
